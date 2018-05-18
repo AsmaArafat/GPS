5a0890b81b501196eea6c165e8b2a0997a92391c
Asma Arafat <asma_arafat2017@hotmail.com>
2018-05-19 01:39:59 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/gps/MainActivity.java
@@ -0,0 +1,304 @@
+package com.example.android.gps;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Network;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    /*
+        private Button b;
+        private TextView t;
+        private LocationManager locationManager;
+        private LocationListener listener;
+        private Button m;*/
+    private LocationManager locationMangaer = null;
+    private LocationListener locationListener = null;
+
+    private Button btnGetLocation = null;
+    private EditText editLocation = null;
+    private ProgressBar pb = null;
+
+    private static final String TAG = "Debug";
+    private Boolean flag = false;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //if you want to lock screen for always Portrait mode
+        setRequestedOrientation(ActivityInfo
+                .SCREEN_ORIENTATION_PORTRAIT);
+
+        pb = (ProgressBar) findViewById(R.id.progressBar1);
+        pb.setVisibility(View.INVISIBLE);
+
+        editLocation = (EditText) findViewById(R.id.editTextLocation);
+
+        btnGetLocation = (Button) findViewById(R.id.btnLocation);
+        btnGetLocation.setOnClickListener((View.OnClickListener) MainActivity.this);
+
+        locationMangaer = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationMangaer.getLastKnownLocation(locationMangaer.NETWORK_PROVIDER);
+        //locationListener.onLocationChanged(location);
+        Toast.makeText(getBaseContext(), "Location changed : Lat: " +
+                        location.getLatitude() + " Lng: " + location.getLongitude(),
+                Toast.LENGTH_SHORT).show();
+        String cityName = null;
+        Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+        List<Address> addresses;
+        try {
+            addresses = gcd.getFromLocation(location.getLatitude(),
+                    location.getLongitude(), 1);
+            if (addresses.size() > 0)
+                System.out.println(addresses.get(0).getLocality());
+            cityName = addresses.get(0).getLocality();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String s = location.getLatitude() + "\n" + location.getLongitude() + "\n\nMy Currrent City is: "
+                + cityName;
+        editLocation.setText(s);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        flag = displayGpsStatus();
+        if (flag) {
+
+            Log.v(TAG, "onClick");
+
+            editLocation.setText("Please!! move your device to" +
+                    " see the changes in coordinates." + "\nWait..");
+
+            pb.setVisibility(View.VISIBLE);
+            locationListener = new MyLocationListener();
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+
+            locationMangaer.requestLocationUpdates(LocationManager
+                    .GPS_PROVIDER, 5000, 2, locationListener);
+
+        } else {
+            alertbox("Gps Status!!", "Your GPS is: OFF");
+        }
+
+    }
+
+    /*----Method to Check GPS is enable or disable ----- */
+    private Boolean displayGpsStatus() {
+        ContentResolver contentResolver = getBaseContext()
+                .getContentResolver();
+        boolean gpsStatus = Settings.Secure
+                .isLocationProviderEnabled(contentResolver,
+                        LocationManager.GPS_PROVIDER);
+        if (gpsStatus) {
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+
+    /*----------Method to create an AlertBox ------------- */
+    protected void alertbox(String title, String mymessage) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Your Device's GPS is Disable")
+                .setCancelable(false)
+                .setTitle("** Gps Status **")
+                .setPositiveButton("Gps On",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // finish the current activity
+                                // AlertBoxAdvance.this.finish();
+                                Intent myIntent = new Intent(
+                                        Settings.ACTION_SECURITY_SETTINGS);
+                                startActivity(myIntent);
+                                dialog.cancel();
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // cancel the dialog box
+                                dialog.cancel();
+                            }
+                        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    /*----------Listener class to get coordinates ------------- */
+    public class MyLocationListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location loc) {
+
+            editLocation.setText("Longitude: " + loc.getLongitude()+ "Latitude: " + loc.getLatitude());
+            pb.setVisibility(View.INVISIBLE);
+            Toast.makeText(getBaseContext(), "Location changed : Lat: " +
+                            loc.getLatitude() + " Lng: " + loc.getLongitude(),
+                    Toast.LENGTH_SHORT).show();
+            String longitude = "Longitude: " + loc.getLongitude();
+            Log.v(TAG, longitude);
+            String latitude = "Latitude: " + loc.getLatitude();
+            Log.v(TAG, latitude);
+
+
+        /*----------to get City-Name from coordinates ------------- */
+                String cityName = null;
+                Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+                List<Address> addresses;
+                try {
+                    addresses = gcd.getFromLocation(loc.getLatitude(),
+                            loc.getLongitude(), 1);
+                    if (addresses.size() > 0)
+                        System.out.println(addresses.get(0).getLocality());
+                    cityName = addresses.get(0).getLocality();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                String s = longitude + "\n" + latitude + "\n\nMy Currrent City is: "
+                        + cityName;
+                editLocation.setText(s);
+            }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+
+
+        }
+    }
+
+
+
+    /* @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        t = (TextView) findViewById(R.id.textView);
+        b = (Button) findViewById(R.id.button);
+        m = findViewById(R.id.button_map);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(locationManager.NETWORK_PROVIDER);
+        onLocationChanged(location);
+
+    }*/
+
+        public void showMap(View view) {
+            Intent mainIntent = new Intent(MainActivity.this, MapsActivity.class);
+            startActivity(mainIntent);
+        }}
+
+
+  /*  @Override
+    public void onLocationChanged(Location location) {
+        double longitude= location.getLongitude();
+        double latitude=location.getLatitude();
+        t.setText("Longitude:" + longitude + "\n" + "Latitude:" + latitude);
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(i);
+
+    }*/
+
+
